15eff37db2acc08ad8a30ec5ead69945ad18d180
Alok Kumar Pandey <alokumarpandey76@gmail.com>
2026-04-06 23:19:00 +0530

    fixed error in proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -1,9 +1,9 @@
-import { auth, clerkMiddleware , createRouteMatcher} from '@clerk/nextjs/server'
+import { clerkMiddleware , createRouteMatcher} from '@clerk/nextjs/server'
 import { NextResponse } from 'next/server';
 
 const isPublicRoute = createRouteMatcher([
   '/signin',
-  'signout',
+  '/signout',
   '/',
   '/home',
 ])
@@ -12,8 +12,8 @@ const isPublicApiRoute = createRouteMatcher([
   '/api/videos'
 ])
 
-export default clerkMiddleware((auth, req) => {
-  const {userId} = auth();
+export default clerkMiddleware(async(auth, req) => {
+  const {userId} = await auth();
   const currentUrl = new URL(req.url);
   const isAccessingDashboard = currentUrl.pathname === '/home';
   const isApiRequest = currentUrl.pathname.startsWith('/api');
